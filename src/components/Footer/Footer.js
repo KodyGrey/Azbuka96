2472463a14d277d0a370dd952088d95e97078fb2
KodyGrey <alexzu2411@gmail.com>
2022-07-12 00:19:46 +0500

    fix: Wrapped footer in card

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 import styles from "./Footer.module.css";
 import arrowDown from "../../public/arrow-down.svg";
+import Card from "../UI/Card";
 
 const Footer = () => {
   const [showingList, setShowingList] = useState({
@@ -20,58 +21,60 @@ const Footer = () => {
   };
 
   return (
-    <footer className={styles["footer"]}>
-      <section
-        className={`${showingList.first && styles["show-list"]} ${
-          styles["information-section"]
-        }`}
-      >
-        <div onClick={onDropDownListHeaderClickHandler} id="first">
-          <h2>О компании</h2>
-          <span className={styles["arrow-down"]}>
-            <Image src={arrowDown} alt="" layout="fill" />
-          </span>
-        </div>
-        <ul>
-          <li>Информация о продавце</li>
-          <li>Адрес и пунуты выдачи</li>
-          <li>Доставка</li>
-          <li>График работы</li>
-        </ul>
-      </section>
-      <section
-        className={`${showingList.second && styles["show-list"]} ${
-          styles["information-section"]
-        }`}
-      >
-        <div onClick={onDropDownListHeaderClickHandler} id="second">
-          <h2>Покупателям</h2>
-          <span className={styles["arrow-down"]}>
-            <Image src={arrowDown} alt="" layout="fill" />
-          </span>
-        </div>
-        <ul>
-          <li>Способы оплаты заказа</li>
-          <li>Скидки</li>
-        </ul>
-      </section>
-      <section
-        className={`${showingList.third && styles["show-list"]} ${
-          styles["information-section"]
-        }`}
-      >
-        <div onClick={onDropDownListHeaderClickHandler} id="third">
-          <h2>Контакты</h2>
-          <span className={styles["arrow-down"]}>
-            <Image src={arrowDown} alt="" layout="fill" />
-          </span>
-        </div>
-        <ul>
-          <li>🖁 +7(343)200-28-80</li>
-          <li>✉ torg5@azbuka96.ru</li>
-        </ul>
-      </section>
-    </footer>
+    <Card>
+      <footer className={styles["footer"]}>
+        <section
+          className={`${showingList.first && styles["show-list"]} ${
+            styles["information-section"]
+          }`}
+        >
+          <div onClick={onDropDownListHeaderClickHandler} id="first">
+            <h2>О компании</h2>
+            <span className={styles["arrow-down"]}>
+              <Image src={arrowDown} alt="" layout="fill" />
+            </span>
+          </div>
+          <ul>
+            <li>Информация о продавце</li>
+            <li>Адрес и пунуты выдачи</li>
+            <li>Доставка</li>
+            <li>График работы</li>
+          </ul>
+        </section>
+        <section
+          className={`${showingList.second && styles["show-list"]} ${
+            styles["information-section"]
+          }`}
+        >
+          <div onClick={onDropDownListHeaderClickHandler} id="second">
+            <h2>Покупателям</h2>
+            <span className={styles["arrow-down"]}>
+              <Image src={arrowDown} alt="" layout="fill" />
+            </span>
+          </div>
+          <ul>
+            <li>Способы оплаты заказа</li>
+            <li>Скидки</li>
+          </ul>
+        </section>
+        <section
+          className={`${showingList.third && styles["show-list"]} ${
+            styles["information-section"]
+          }`}
+        >
+          <div onClick={onDropDownListHeaderClickHandler} id="third">
+            <h2>Контакты</h2>
+            <span className={styles["arrow-down"]}>
+              <Image src={arrowDown} alt="" layout="fill" />
+            </span>
+          </div>
+          <ul>
+            <li>🖁 +7(343)200-28-80</li>
+            <li>✉ torg5@azbuka96.ru</li>
+          </ul>
+        </section>
+      </footer>
+    </Card>
   );
 };
 

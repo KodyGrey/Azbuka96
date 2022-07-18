9c9cfe09c6708536cda7da3bff6a8bb912de7975
KodyGrey <alexzu2411@gmail.com>
2022-07-18 22:14:56 +0500

    feat: added image section on index page

---
+++ src/components/InfoElements/IndexImageSection.js
@@ -0,0 +1,28 @@
+import Image from "next/dist/client/image";
+
+import styles from "./IndexImageSection.module.css";
+import indexImage from "../../public/index-page-image.svg";
+import indexTextPc from "../../public/index-page-text-pc.svg";
+import indexTextMobile from "../../public/index-page-text-mobile.svg";
+
+const IndexImageSection = () => {
+  const text = `Учебники
+  Рабочие тетради
+  Методическая литература`;
+
+  return (
+    <section className={styles["section"]}>
+      <span className={styles["image"]}>
+        <Image src={indexImage} alt="" layout="fill" />
+      </span>
+      <span className={styles["text-pc"]}>
+        <Image src={indexTextPc} alt={text} layout="fill" />
+      </span>
+      <span className={styles["text-mobile"]}>
+        <Image src={indexTextMobile} alt={text} layout="fill" />
+      </span>
+    </section>
+  );
+};
+
+export default IndexImageSection;
---
+++ src/stories/IndexImageSection.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import IndexImageSection from "../components/InfoElements/IndexImageSection";
+
+export default {
+  title: "Index Image Section",
+  component: IndexImageSection,
+};
+
+const Template = (args) => <IndexImageSection {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};

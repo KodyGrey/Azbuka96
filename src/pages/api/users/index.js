b658ac17e3e8c0c1baa38d2c3390f820e6b8a91b
KodyGrey <alexzu2411@gmail.com>
2022-08-02 12:57:52 +0500

    fix: added protection from unathorized and non-admin users

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,23 +1,37 @@
 import clientPromise from "../../../lib/mongodbClient";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 // Returns promise, so server understand when
 // function ends gathering and sending information
 export default async function handler(req, res) {
   return new Promise((resolve) => {
-    clientPromise
-      .then((client) => {
-        const collection = client.db().collection("users");
-        collection.find({}).toArray((err, array) => {
-          if (err) res.status(500).json({ error: err.toString() });
-          else {
-            res.status(200).json(array);
-          }
+    unstable_getServerSession(req, res, authOptions).then((session) => {
+      if (!session) {
+        res.status(401).json({ error: "Unauthorized" });
+        return resolve();
+      } else if (!session.user.is_admin) {
+        res
+          .status(403)
+          .json({ error: "You have not enough permissions for that" });
+        return resolve();
+      }
+
+      clientPromise
+        .then((client) => {
+          const collection = client.db().collection("users");
+          collection.find({}).toArray((err, array) => {
+            if (err) res.status(500).json({ error: err.toString() });
+            else {
+              res.status(200).json(array);
+            }
+            return resolve();
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.toString() });
           return resolve();
         });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.toString() });
-        return resolve();
-      });
+    });
   });
 }

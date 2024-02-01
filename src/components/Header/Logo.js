70bdf4a2257d1cc616fde12080be84afc13e27e6
KodyGrey <alexzu2411@gmail.com>
2024-02-01 11:18:28 +0300

    fix: changed the logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import logoPic from "../../public/azbuka96.svg";
+import logoPic from "../../public/logo.svg";
 
 const Logo = () => {
   return (
     <Link href="/" legacyBehavior>
-      <a style={{ height: 40 }}>
-        <Image width={135} height={40} src={logoPic} alt="Азбука96" />
+      <a style={{ height: 60 }}>
+        <Image width={200} height={60} src={logoPic} alt="Азбука96" />
       </a>
     </Link>
   );
